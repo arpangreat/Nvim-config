a81a4976e4ff96e696613f684e8bfff9088af914
arpangreat <arpanthegreat41@gmail.com>
2021-03-24 01:28:48 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Nvim-config
+# A Backup for nvin configuration
+# Because I love my Config more than anything
+
---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp ~/dotfiles/nvim/init.lua ~/Nvim-config
+cp ~/dotfiles/nvim/init.vim.bk ~/Nvim-config
+cp ~/dotfiles/nvim/init.vim.last ~/Nvim-config
+cp ~/dotfiles/nvim/lua ~/Nvim-config -r
+cp ~/dotfiles/nvim/plugin ~/Nvim-config -r
